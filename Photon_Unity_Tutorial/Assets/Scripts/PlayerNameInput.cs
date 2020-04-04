9bead9c2a3c0405f25b54dab5f328e5e839af51f
Caleb Smith <calebsmith12890@gmail.com>
2020-04-03 21:13:09 -0700

    Filled Scripts for lobby
    Scene_Main and Scene_MainMenu finished, and panels created to allow player to start or join lobby.

--- Photon_Unity_Tutorial/Assets/Scripts/PlayerNameInput.cs
+++ Photon_Unity_Tutorial/Assets/Scripts/PlayerNameInput.cs
@@ -1,18 +1,48 @@
-﻿using System.Collections;
+﻿using Photon.Pun;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerNameInput : MonoBehaviour
 {
+
+    [SerializeField] private InputField nameInputField = null;
+    [SerializeField] private Button continueButton = null;
+    private const string PlayerPrefsNameKey = "PlayerName";
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private void Start() => SetUpInputField();
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void SetUpInputField()
+    {
+        if(!PlayerPrefs.HasKey(PlayerPrefsNameKey)) { return;}
+
+        string defaultName = PlayerPrefs.GetString(PlayerPrefsNameKey);
+
+        nameInputField.text = defaultName;
+
+        SetPlayerName(defaultName);
+    }
+
+    public void SetPlayerName(string name)
+    {
+        continueButton.interactable = !string.IsNullOrEmpty(name);
+    }
+
+    public void SavePlayerName()
+    {
+        string playerName = nameInputField.text;
+
+        PhotonNetwork.NickName = playerName;
+
+        PlayerPrefs.SetString(PlayerPrefsNameKey, playerName);
     }
 }
